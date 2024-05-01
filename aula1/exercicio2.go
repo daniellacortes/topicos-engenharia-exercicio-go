b6ebfadcd4a9f381c96c408cf3c70a84d78911ea
Daniella Brito Côrtes <84427443+daniellacortes@users.noreply.github.com>
2024-04-30 21:15:30 -0300

    Exercícios 1 e 2
    Hello World e contar string

---
+++ aula1/exercicio2.go
@@ -0,0 +1,31 @@
+package main
+
+import
+(
+	"errors"
+	"fmt"
+)
+
+func main ()
+{
+	str := "Hello, World!"
+	lengh, err := CountString (str)
+
+	if err != nil
+	{
+		fmt.Println ("Erro:", err)
+	} else
+	{
+		fmt.Println ("Tamanho da String: ", lengh)
+	}
+
+}
+
+func CountString (s string) (int, error)
+{
+	if s == " "
+	{
+		return 0, errors.New ("String está vazia")
+	}
+	return len (s), nil
+}
